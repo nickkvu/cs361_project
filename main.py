dcbb2117fedaf235bebdc877dad176b3171904a5
Nicholas Vu <vuni@oregonstate.edu>
2025-02-09 21:50:16 -0800

    User Story 1 (1/2): Converter is functional but edge cases are not implemented yet

--- main.py
+++ main.py
@@ -3,6 +3,32 @@
 
 import os
 
+def convert_dec_to_bin(dec) :
+    result = ""
+
+    while dec > 0 :
+        result = str(dec % 2) + result
+        dec = dec // 2
+
+    return result
+
+def file_convert_dec_to_bin(filename) :
+    # store an array of all the numbers read in the file
+    nums = []
+    binarys = []
+
+    #read the file line by line and store into the initialzed array
+    file = open(filename, "r")
+    for line in file :
+        nums.append(int(line))
+    
+    # use the array to get each number and convert it into binary
+    for i in nums :
+        binarys.append(convert_dec_to_bin(i))
+
+    # return the array of numbers and array of binary values
+    return nums, binarys
+
 # Home Page Screen:
 def home_page() :
     # print home screen messages to terminal
@@ -34,7 +60,31 @@ def decimal_to_binary() :
                                                                      |___/                                                
     """)
     print("Would you like to enter a decimal to the CLI or enter the name of a file: \n")
-    print("1. Manual Input\n2. File Input")
+    print("1. Manual Input\n2. File Input\n")
+    
+    input_process = int(input("Processing Option: "))       #select how to use the converter
+
+    if (input_process == 1) :
+        # Manually input a decimal to convert into binary
+        print("\nEnter a DECIMAL to convert it into its BINARY value!\n")
+        decimal_num = int(input("[Decimal]: "))
+
+        #Run the entered number into the decimal-to-binary converter
+        result = convert_dec_to_bin(decimal_num)
+
+        print("[Binary]: " + result)
+    
+    elif (input_process == 2) :
+        # use a file based input to convert a list of decimal numbers to binary
+        print("\nEnter the file name you wish to get BINARY values from!\n")
+        filename = input("[File]: ")
+
+        dec, bin = file_convert_dec_to_bin(filename)    #get a list of the decimal numbers and the corresponding binary numbers
+
+        print("\n[List of Conversions] from " + filename)
+        for i in range(len(dec)) :
+            print("[Decimal]: " + str(dec[i]) + "\n[Binary]: " + str(bin[i]) + "\n")
+            
 
 def binary_to_decimal() :
     print(r""" 
@@ -48,7 +98,8 @@ def binary_to_decimal() :
                                |___/                                                                                                                                  
     """)
     print("Would you like to enter a decimal to the CLI or enter the name of a file: \n")
-    print("1. Manual Input\n2. File Input")
+    print("1. Manual Input\n2. File Input\n")
+
 
 def exit_page() :
     print(r""" 
@@ -60,25 +111,24 @@ def exit_page() :
      |______/_/\_\_|\__|
                                                                                                                                                                 
     """)
-    print("Are you sure you want to exit the program? (y/n)")
+    print("Are you sure you want to exit the program? (y/n)\n")
 
 
 # Main Function:
 home_page()
 
-input = int(input("User's Input: "))    # get user's entered input
+nav = int(input("User's Input: "))    # get user's entered input
 
-if(input == 1) :
+if(nav == 1) :
     # Clear terminal and navigate to Decimal-to-Binary Converter Sreen
     os.system('cls' if os.name == 'nt' else 'clear')    #clear the terminal to make cleaner interface
     decimal_to_binary()
 
-elif (input == 2) :
+elif (nav == 2) :
     # Clear terminal and navigate to Binary-to-Decimal Converter Sreen
     os.system('cls' if os.name == 'nt' else 'clear')    #clear the terminal to make cleaner interface
     binary_to_decimal()
-
-elif(input == 3) :
+elif(nav == 3) :
     # exit the program
     os.system('cls' if os.name == 'nt' else 'clear')    #clear the terminal to make cleaner interface
     exit_page()
